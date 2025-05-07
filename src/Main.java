dd77cac49ce88d7856c5407285389a396d246c58
DeVector <diniz.vito@gmail.com>
2025-05-07 19:31:59 -0300

    Add reflection para pegar o nome da annotation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import br.com.devector.Tabela;
+import br.com.devector.TabelaAnnotation;
+
+import java.lang.reflect.Field;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
+
+        Tabela tabela = new Tabela();
+        Class instClass = tabela.getClass();
+
+        Field[] fields = instClass.getDeclaredFields();
+
+        for (Field field1 : fields){
+            TabelaAnnotation fieldName = field1.getAnnotation(TabelaAnnotation.class);
+            System.out.println("Nome da anotação: " + fieldName.nomeDaTabela().toString());
+        }
 
     }
 }
